f0e036ad98aed4a157039656d3442fd5afd45ea2
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2020-01-07 12:43:16 +0900

    Natural Language Processing Project Files
    Pushed all of the project files.

---
+++ natural-language-processing/README.md
@@ -0,0 +1,38 @@
+# Project Instructions
+
+Project Objectives:
+- Set-up Webpack
+- Sass styles
+- Webpack Loaders and Plugins
+- Create layouts and page design
+- Service workers
+- Use APIs and create external url requests
+
+## Getting started
+
+`cd` into the project folder and run:
+- `npm install`
+
+## Setting up the API
+
+Create a new ```.env``` file in the root of your project and dill the .env file with your API keys like this:
+```
+API_ID=**************************
+API_KEY=**************************
+```
+
+## Starting the API Server
+
+Run `npm run start` to run the API server
+
+## Starting the Client - Development
+
+Run `npm run build-dev` to run the client
+
+## Starting the Client - Production
+
+Run `npm run build-prod` to run the client
+
+## Run Jest Test
+
+Run `npm run test` to run the tests
---
+++ natural-language-processing/webpack.dev.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate file removal
+            dry: true,
+            // Write logs to console
+            verbose: true,
+            // Automatically remove all unused webpack assets upon rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new WorkboxPlugin.GenerateSW()
+    ]
+}
